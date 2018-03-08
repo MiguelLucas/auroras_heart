d33d787ede1c75b64c18a7d5ab2ddc02c5c22127
Inês de Sousa Caldas <ines.sousacaldas@gmail.com>
2018-03-08 19:28:44 +0000

    elements

---
+++ Unity/Aurora's Heart/Assets/Scripts/Elements/Element.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour {
+
+    [SerializedField]
+    private ElementType elemType;
+
+
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
+
+public enum ElementType
+{
+    ice,
+    fire,
+    earth,
+    death,
+    forest
+}
